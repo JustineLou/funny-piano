da402a91207ab8161f18a7c1d2d21864c4ea7567
JustineLou <71087008+JustineLou@users.noreply.github.com>
2020-10-22 14:34:51 +0200

    'transformation-en-Vue'

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
-  <div id="app">
-    <h1>Bon courage !</h1>
+  <div id="app" class="principal-container">
+    <v-header></v-header>
+    <v-emoji></v-emoji>
+    <v-piano></v-piano>
   </div>
 </template>
 
 <script>
-
+import VHeader from '@/components/VHeader'
+import VEmoji from '@/components/VEmoji'
+import VPiano from  '@/components/VPiano'
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  }
-}
+    VHeader,
+    VEmoji,
+    VPiano,
+  },
+};
 </script>
+
+<style lang="postcss"  scoped>
+  .principal-container{
+    background: linear-gradient(rgba(255, 157, 108, 1), rgba(187, 78, 117, 1));
+    background-size: 100%;
+    position: relative;
+    width: 1440px;
+    height: 768px;
+}
+</style>
